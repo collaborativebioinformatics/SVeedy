1fc1eb50d8cfc565947012dbf4fc6c7ec5eaf30d
Van Huynh <63719646+van-huynh@users.noreply.github.com>
2024-08-30 15:11:02 -0500

    Create Chr3_Analysis.R

---
+++ Rscripts/Chr3_Analysis.R
@@ -0,0 +1,16 @@
+
+
+# Read tsv file
+Chr03 <- read.table(file='adotto_variants.grch38.sqoff.chr3.above50bp.truvari_collapsed_oc_ready.vcf.tsv', skip = 6, sep = '\t', header =  FALSE, fill = TRUE)
+header <- read.table(file='adotto_variants.grch38.sqoff.chr1.above50bp.truvari_collapsed_oc_ready.vcf.tsv', sep = '\t', skip = 2, nrows =1,header =  TRUE, fill = TRUE)
+colnames(Chr03) <- unlist(header)
+
+# Count clinical significant variants
+nrow(Chr03[Chr03$`Clinical Significance` == "Pathogenic",]) # returns 3
+nrow(Chr03[Chr03$`Clinical Significance` == "Likely pathogenic",]) # returns 0
+
+# Subset Pathogenic variants
+Chr3_Pathogenic <- Chr03[Chr03$`Clinical Significance` == 'Pathogenic', ]
+
+# Write data frame into csv file
+write.csv(Chr3_Pathogenic, "Chr3_Pathogenic.csv")
